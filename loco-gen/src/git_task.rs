eb5880a9c8049089c295a80e94c4f1177e6df8c0
flopetautschnig (floscodes) <mail@floscodes.net>
2025-07-31 17:57:48 +0200

    refactor: split parts in smaller functions

--- loco-gen/src/git_task.rs
+++ loco-gen/src/git_task.rs
@@ -17,59 +17,11 @@ pub fn fetch_and_generate(
     appinfo: &AppInfo,
 ) -> Result<GenerateResults> {
     if let Some(git_url) = git_url {
-        println!("Fetching task from git repository: {}", git_url);
+        println!("Cloning task from git repository: {}", git_url);
         let _repo = clone_repo(git_url, Path::new("./tasks"))?;
-        let git_path = git_url
-            .rsplit("/")
-            .next()
-            .ok_or(Error::Message("Failed to get git repo name".to_string()))?;
-        let path_str = format!("./tasks/{}", git_path);
-        let git_dir = Path::new(&path_str);
-        let config_path = git_dir.join(CONFIG_FILE);
-        println!(
-            "Check if loco-task.toml exists at: {}",
-            config_path.display()
-        );
-        // Check if the configuration file exists
-        if !config_path.exists() {
-            return Err(Error::Message(format!(
-                "{} not found in the repository",
-                CONFIG_FILE
-            )));
-        }
-        let config_file = std::fs::read_to_string(config_path.clone())
-            .map_err(|e| Error::Message(format!("Failed to read {}: {}", CONFIG_FILE, e)))?;
-        println!("Parsing loco-task.toml");
-        let config_toml: Value = toml::from_str(&config_file)
-            .map_err(|e| Error::Message(format!("Failed to parse {}: {}", CONFIG_FILE, e)))?;
-        let task_name = config_toml
-            .get("package")
-            .and_then(|v| v.get("name"))
-            .ok_or(Error::Message(format!(
-                "Package name missing in {}. Task name is required.",
-                CONFIG_FILE
-            )))?;
-        let task_name_path_string = format!("./tasks/{}", task_name.to_string());
-        println!(
-            "Renaming git directory to task name: {}",
-            task_name_path_string
-        );
-        let renamed_git_dir = Path::new(&task_name_path_string);
-        std::fs::rename(git_dir, renamed_git_dir).map_err(|e| {
-            Error::Message(format!(
-                "Failed to rename git directory to task name: {}",
-                e
-            ))
-        })?;
-        remove_project_dep_from_cargo_toml(config_file, &config_path).map_err(|e| {
-            Error::Message(format!(
-                "Failed to edit Cargo.toml for updating pkg_root dependency: {}",
-                e
-            ))
-        })?;
-        let app_name = appinfo.app_name.as_str();
-        println!("Rendering template files");
-        render_git_task(rrgen, task_name.to_string(), app_name)
+        println!("Processing the cloned repository");
+        process_repo(rrgen, git_url, appinfo)
+            .map_err(|e| Error::Message(format!("Failed to process git repository: {}", e)))
     } else {
         Err(Error::Message(
             "Error while trying to generate task from git - no valid git url provided!".to_string(),
@@ -81,6 +33,60 @@ fn clone_repo(git_url: &str, path: &Path) -> Result<()> {
     git2::Repository::clone(git_url, path)
         .map_err(|e| Error::Message(format!("Failed to clone git repository: {}", e)))?;
     Ok(())
+}
+
+fn process_repo(rrgen: &RRgen, git_url: &str, appinfo: &AppInfo) -> Result<GenerateResults> {
+    let git_path = git_url
+        .rsplit("/")
+        .next()
+        .ok_or(Error::Message("Failed to get git repo name".to_string()))?;
+    let path_str = format!("./tasks/{}", git_path);
+    let git_dir = Path::new(&path_str);
+    let config_path = git_dir.join(CONFIG_FILE);
+    println!(
+        "Check if loco-task.toml exists at: {}",
+        config_path.display()
+    );
+    // Check if the configuration file exists
+    if !config_path.exists() {
+        return Err(Error::Message(format!(
+            "{} not found in the repository",
+            CONFIG_FILE
+        )));
+    }
+    let config_file = std::fs::read_to_string(config_path.clone())
+        .map_err(|e| Error::Message(format!("Failed to read {}: {}", CONFIG_FILE, e)))?;
+    println!("Parsing loco-task.toml");
+    let config_toml: Value = toml::from_str(&config_file)
+        .map_err(|e| Error::Message(format!("Failed to parse {}: {}", CONFIG_FILE, e)))?;
+    let task_name = config_toml
+        .get("package")
+        .and_then(|v| v.get("name"))
+        .ok_or(Error::Message(format!(
+            "Package name missing in {}. Task name is required.",
+            CONFIG_FILE
+        )))?;
+    let task_name_path_string = format!("./tasks/{}", task_name.to_string());
+    println!(
+        "Renaming git directory to task name: {}",
+        task_name_path_string
+    );
+    let renamed_git_dir = Path::new(&task_name_path_string);
+    std::fs::rename(git_dir, renamed_git_dir).map_err(|e| {
+        Error::Message(format!(
+            "Failed to rename git directory to task name: {}",
+            e
+        ))
+    })?;
+    remove_project_dep_from_cargo_toml(config_file, &config_path).map_err(|e| {
+        Error::Message(format!(
+            "Failed to edit Cargo.toml for updating pkg_root dependency: {}",
+            e
+        ))
+    })?;
+    let app_name = appinfo.app_name.as_str();
+    println!("Rendering template files");
+    render_git_task(rrgen, task_name.to_string(), app_name)
 }
 
 fn render_git_task(rrgen: &RRgen, task_name: String, app_name: &str) -> Result<GenerateResults> {
